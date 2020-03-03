ac6f281306c5cd34bb29d044a839d19258671e24
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-03 04:35:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,8 +46,8 @@ def consolidate_cart(cart)
     new_cart_item = cart[counter][:item]
     if new_cart_item != nil
       # item_details[:count] = 1
-      new_cart_item[:count] += 1
     binding.pry
+      new_cart_item[:count] += 1
       new_cart_item = item_details[:count]
     else
       new_cart_item = {  
