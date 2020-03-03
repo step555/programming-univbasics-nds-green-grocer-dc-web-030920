5be164ad2e83ec75e8f45780c9ea9e484d8c7cb0
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-03 05:36:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def consolidate_cart(cart)
   counter = 0
   cart.each do |item_details|
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if cart[counter].has_key?(:count)
+    if new_cart_item.has_key?(:count)
       # cart[counter][:count]
       # item_details[:count] += 1 nd
       cart[counter][:count] += 1
