93d8e47e9d8f16a9c49f57b03122870c861f617b
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-03 03:22:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,16 +57,6 @@ def consolidate_cart(cart)
   #   end
   # end
   # new_cart
-  new_cart = {} 
-  cart.each do |items_array| 
-    items_array.each do |item, attribute_hash| 
-      new_cart[item] ||= attribute_hash 
-      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
-      new_cart[item][:count] = 1 
-  end 
-end 
-new_cart 
-
 end
 
 # def apply_coupons(cart, coupons)
