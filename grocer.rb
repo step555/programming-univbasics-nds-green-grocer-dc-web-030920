04c118eda2dec0e41a0c61d3788d2f2845e7d8f7
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-03 04:29:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def consolidate_cart(cart)
   counter = 0
   cart.each do |item_details|
     new_cart_item = cart[counter][:item]
-    binding.pry
+    # binding.pry
     if item_details != nil
       # item_details[:count] = 1
       item_details[:count] += 1
