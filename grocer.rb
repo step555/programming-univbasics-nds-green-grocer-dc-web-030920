ecad11cbf23c1f6fb6888e598af6e8f8590bd4e8
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-03 01:49:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,8 +42,8 @@ def consolidate_cart(cart)
   # new_cart
   new_cart = []
   cart.each do |item_details|
-    new_cart_item = find_item_by_name_in_collection(cart[item_details][:item], new_cart)
     binding.pry
+    new_cart_item = find_item_by_name_in_collection(cart[item_details][:item], new_cart)
     if new_cart_item != nil
       new_cart_item[:count] += 1
     else
